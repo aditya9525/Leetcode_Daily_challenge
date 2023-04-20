993f5f2f199b5285531af9ac0fa4b700807eb668
Aditya kumar <99443350+aditya9525@users.noreply.github.com>
2023-04-21 02:30:20 +0530

    Update Maximum Width of Binary Tree.cpp

--- Maximum Width of Binary Tree.cpp
+++ Maximum Width of Binary Tree.cpp
@@ -1,24 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-
-        if(root==NULL)
-            return 0;
         long long ans=0;
+        if(root==NULL){
+            return 0;
+        }
         queue<pair<TreeNode*, int>>q;
         q.push({root, 0});
         while(!q.empty()){
-            int l=q.size();
-            long long la=q.back().second, f=q.front().second;
-            for(int i=0; i<l; i++){
+            int q_size=q.size();
+            long long lastIndex=q.back().second, firstIndex=q.front().second;
+            for(int i=0; i<q_size; i++){
                 TreeNode* temp;
                 temp=q.front().first;
-                long long ind=q.front().second;
-                q.pop();   
-                if(temp->left!=NULL){q.push({temp->left, ind*2+1});}
-                if(temp->right!=NULL){q.push({temp->right, ind*2+2});}
+                long long index=q.front().second;
+                q.pop();
+                if(temp->left!=NULL){
+                    q.push({temp->left, index*2+1});
+                }
+                if(temp->right!=NULL){
+                    q.push({temp->right, index*2+2});
+                }
             }
-            ans=max(ans, la-f+1);
+            ans = max(ans, lastIndex - firstIndex + 1);
         }
         return ans;
     }
